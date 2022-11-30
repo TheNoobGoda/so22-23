b6bd60c89fd49188d9e31696099ee0880f70ba1b
TheNoobGoda <davidespinhamarques@gmail.com>
2022-11-30 23:57:33 +0000

    all processes created

--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -16,10 +16,12 @@ int main(int argc, char *argv[])
         printf("usage: tokenring numberProcesses probability time\n");
         return EXIT_FAILURE;
     }
-    float p = strtof(argv[2], NULL)*100;
+    float p = strtof(argv[2], NULL) * 100;
     int t = strtol(argv[3], NULL, 10);
-    
+    int n = atoi(argv[1]);
+
     pid_t pid;
+    /*
     char name1[13];
     char name2[13];
 
@@ -29,13 +31,13 @@ int main(int argc, char *argv[])
     mkfifo(name1,0666);
     mkfifo(name2,0666);
     int token = 0;
+    srand(time(0));
     pid = fork();
     if (pid==0){
         int fd1 = open(name1,O_WRONLY);
         int fd2 = open(name2,O_RDONLY);
         while(read(fd2,(char*)&token,sizeof(token))>0){
             token++;
-            srand(time(0));
             int num = rand()%100;
             if (num <= p){
                 printf("[%d] lock on token (val = %d)\n",getpid(),token);
@@ -43,7 +45,7 @@ int main(int argc, char *argv[])
                 printf("[%d] unlock on token\n",getpid());
                 fflush(stdout);
             }
-            
+
             write(fd1,(char*)&token,sizeof(token));
         }
 
@@ -53,7 +55,6 @@ int main(int argc, char *argv[])
         write(fd2,(char*)&token,sizeof(token));
         while(read(fd1,(char*)&token,sizeof(token))>0){
             token++;
-            srand(time(0));
             int num = rand()%100;
             if (num <= p){
                 printf("[%d] lock on token (val = %d)\n",getpid(),token);
@@ -61,9 +62,78 @@ int main(int argc, char *argv[])
                 printf("[%d] unlock on token\n",getpid());
                 fflush(stdout);
             }
-            
+
             write(fd2,(char*)&token,sizeof(token));
         }
     }
+    */
+    int proc = 1;
+    for (int i = 1; i < n; i++)
+    {
+        char name[13];
+        sprintf(name, "pipe%dto%d", i, i + 1);
+        mkfifo(name, 0666);
+    }
+    char name[13];
+    sprintf(name, "pipe%dto%d", n, 1);
+    mkfifo(name, 0666);
+    int token = 0;
+    srand(time(0));
+    for (int i = 2; i <= n; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+        {
+            proc = i;
+            break;
+        }
+    }
+
+    char name1[13];
+    char name2[13];
+
+    if (proc == n)
+    {
+        sprintf(name1, "pipe%dto%d", proc, 1);
+        sprintf(name2, "pipe%dto%d", proc - 1, proc);
+    }
+    else
+        sprintf(name1, "pipe%dto%d", proc, proc + 1);
+    if (proc == 1)
+    {
+        sprintf(name2, "pipe%dto%d", n, 1);
+    }
+    else
+    {
+        sprintf(name2, "pipe%dto%d", proc - 1, proc);
+    }
+
+    fflush(stdout);
+
+    int fd2 = open(name2, O_RDONLY | O_NONBLOCK);
+    int fd1 = open(name1, O_WRONLY);
+
+
+    if (proc == 1)
+        write(fd1, (char *)&token, sizeof(token));
+    while (1)
+    {
+        int bytes = 0;
+        while (bytes <=0){
+            bytes = read(fd2, (char *)&token, sizeof(token));
+        }
+        token++;
+        int num = rand() % 100;
+        if (num <= p)
+        {
+            printf("[%d] lock on token (val = %d)\n", getpid(), token);
+            sleep(t);
+            printf("[%d] unlock on token\n", getpid());
+            fflush(stdout);
+        }
+
+        write(fd1, (char *)&token, sizeof(token));
+    }
+
     return 0;
 }
