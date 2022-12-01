1968fadf8e20f46daefa225bedd261b34b8056a2
YTheNoobGoda <davidespinhamarques@gmail.com>
2022-12-01 18:30:36 +0000

    removed unnecessary files from q3

--- Q3/tokenring.c
+++ Q3/tokenring.c
@@ -21,52 +21,6 @@ int main(int argc, char *argv[])
     int n = atoi(argv[1]);
 
     pid_t pid;
-    /*
-    char name1[13];
-    char name2[13];
-
-    sprintf(name1,"pipe%dto%d",1,2);
-    sprintf(name2,"pipe%dto%d",2,1);
-
-    mkfifo(name1,0666);
-    mkfifo(name2,0666);
-    int token = 0;
-    srand(time(0));
-    pid = fork();
-    if (pid==0){
-        int fd1 = open(name1,O_WRONLY);
-        int fd2 = open(name2,O_RDONLY);
-        while(read(fd2,(char*)&token,sizeof(token))>0){
-            token++;
-            int num = rand()%100;
-            if (num <= p){
-                printf("[%d] lock on token (val = %d)\n",getpid(),token);
-                sleep(t);
-                printf("[%d] unlock on token\n",getpid());
-                fflush(stdout);
-            }
-
-            write(fd1,(char*)&token,sizeof(token));
-        }
-
-    }else {
-        int fd1 = open(name1,O_RDONLY);
-        int fd2 = open(name2,O_WRONLY);
-        write(fd2,(char*)&token,sizeof(token));
-        while(read(fd1,(char*)&token,sizeof(token))>0){
-            token++;
-            int num = rand()%100;
-            if (num <= p){
-                printf("[%d] lock on token (val = %d)\n",getpid(),token);
-                sleep(t);
-                printf("[%d] unlock on token\n",getpid());
-                fflush(stdout);
-            }
-
-            write(fd2,(char*)&token,sizeof(token));
-        }
-    }
-    */
     int proc = 1;
     for (int i = 1; i < n; i++)
     {
